bd88ecc2c0943249c0911168781c9e0d79aeab62
Sergei Kamianetskii <i@ksergey.ru>
2023-03-03 09:56:56 +0300

    Добавлен метод печати массива

--- MatrixFindNumber/Program.cs
+++ MatrixFindNumber/Program.cs
@@ -10,6 +10,19 @@ void FillMatrix(int[,] matrix, int min, int max)
   }
 }
 
+void Print(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
 
 void FindNumberInMatrix(int[,] matrix, int n)
 {
